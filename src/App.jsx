407fcbbc65986c398ca1f1c7c2d0118c2166aaef
Joshua Richter <joshuawiidrichter@gmail.com>
2025-04-14 17:06:53 +0200

    Added validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import CounterDisplay from './components/CounterDisplay.jsx';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
   const increment = () => setCount((num) => num + 1);
-  const decrement = () => setCount((num) => num - 1);
+  const decrement = () => setCount((num) => (num > 0 ? num - 1 : 0));
 
   return (
     <>
