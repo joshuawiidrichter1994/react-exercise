e88c8d72ca88601e90f3588b50f5c47c176f9ea6
Joshua Richter <joshuawiidrichter@gmail.com>
2025-04-14 18:03:54 +0200

    Added form and handleSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,21 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const increment = () => setCount((num) => num + 1);
-  const decrement = () => setCount((num) => (num > 0 ? num - 1 : 0));
+  const [name, setName] = useState('');
+  const [thankYouMessage, setThankYouMessage] = useState('');
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count > 0 ? count - 1 : 0);
+  const reset = () => setCount(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.trimEnd()) {
+      setThankYouMessage(`${name}, thanks for using the counter!`);
+    } else {
+      setThankYouMessage('');
+    }
+  };
 
   return (
     <>
@@ -13,6 +26,18 @@ function App() {
         <CounterDisplay count={count} />
         <button onClick={increment}>Inrement</button>
         <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">Submit name</button>
+          <h1>{thankYouMessage}</h1>
+        </form>
       </div>
     </>
   );
